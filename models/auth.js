3277375ecdd4a88f503fd7521a549a6381c87fe6
Chertan Vladislav <ketface13@gmail.com>
2026-03-03 19:46:37 +0200

    Authorization added (without roles)

---
+++ models/auth.js
@@ -0,0 +1,44 @@
+import { auth } from "../services/firebaseConfig.js";
+
+import {
+    signInWithEmailAndPassword,
+    onAuthStateChanged,
+    signOut
+} from "https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+
+
+// Вход в систему 
+const loginBtn = document.getElementById("loginBtn");
+
+if (loginBtn) {
+    loginBtn.addEventListener("click", async () => {
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            window.location.href = "./index.html";
+        } catch (error) {
+            document.getElementById("errorMessage").innerText = error.message;
+        }
+    });
+}
+
+
+// Защита обхода 
+onAuthStateChanged(auth, (user) => {
+    if (!user && window.location.pathname.includes("index.html")) {
+        window.location.href = "./login.html";
+    }
+});
+
+
+// Выход из системы 
+const logoutBtn = document.getElementById("logoutBtn");
+
+if (logoutBtn) {
+    logoutBtn.addEventListener("click", async () => {
+        await signOut(auth);
+        window.location.href = "./login.html";
+    });
+}
---
+++ services/firebaseConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from 
+"https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+
+
+import { getAnalytics } from 
+"https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+
+
+import { getAuth } from 
+"https://www.gstatic.com/firebasejs/12.10.0/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAEbaqxardPM5YF9ip0HScdEiOVoip1zXU",
+    authDomain: "practikach.firebaseapp.com",
+    projectId: "practikach",
+    storageBucket: "practikach.firebasestorage.app",
+    messagingSenderId: "1066273688579",
+    appId: "1:1066273688579:web:2d681e1ee88a0221245236",
+    measurementId: "G-NSZJN9K93T"
+};
+
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+export const auth = getAuth(app);
